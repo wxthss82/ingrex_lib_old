28a3155b95502845dd63037a9a347f4411b3eceb
wangxin <wxthss82@gmail.com>
2017-01-21 04:14:10 +0800

    try to fix not all arguments converted during string formatting

--- comm.py
+++ comm.py
@@ -155,7 +155,7 @@ def main():
                     # print(u'{} {}'.format(message.time, message.text.decode('unicode-escape')))
                     # insert into database
                     cursor.execute("INSERT INTO MESSAGE (GUID,LAT,LNG) \
-                                 VALUES (%s,%d,%d)", (message.guid,
+                                 VALUES (%s,%lld,%lld)", (message.guid,
                                                                   message.lat,
                                                                   message.lng,
                                                                   ));
