0c0b1c8336ebce06b0ef5d0c7a8b7d545c70344d
wangxin <wxthss82@gmail.com>
2017-01-21 02:12:34 +0800

    fixed bug

--- ingrex/intel.py
+++ ingrex/intel.py
@@ -2,10 +2,6 @@
 import requests
 import re
 import json
-import ssl
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.poolmanager import PoolManager
-
 
 class Intel(object):
     "main class with all Intel functions"
@@ -34,16 +30,12 @@ class Intel(object):
 
     def refresh_version(self):
         "refresh api version for request"
-        s = requests.Session()
-        s.mount('https://www.ingress.com/intel', SSLAdapter())
-        request = requests.get('https://www.ingress.com/intel', headers=self.headers, verify=False)
+        request = requests.get('https://www.ingress.com/intel', headers=self.headers)
         self.version = re.findall(r'gen_dashboard_(\w*)\.js', request.text)[0]
 
     def fetch(self, url, payload):
         "raw request with auto-retry and connection check function"
         payload['v'] = self.version
-        s = requests.Session()
-        s.mount(url, SSLAdapter())
         request = requests.post(url, data=json.dumps(payload), headers=self.headers)
         return request.json()['result']
 
@@ -130,17 +122,3 @@ class Intel(object):
 
 if __name__ == '__main__':
     pass
-
-
-class SSLAdapter(HTTPAdapter):
-    '''An HTTPS Transport Adapter that uses an arbitrary SSL version.'''
-    def __init__(self, ssl_version=None, **kwargs):
-        self.ssl_version = ssl_version
-
-        super(SSLAdapter, self).__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block,
-                                       ssl_version=self.ssl_version)
